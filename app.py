1d5dc746f20bd71a8ffd1220136270f421238579
ZaytsevNS <fatalbest@mail.ru>
2021-05-26 15:54:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def success(name, start_number, finish_number, quantity_number, random_number, p
     sorted_number = sorted_number, info_platform = info_platform, info_sys_description = info_sys_description, info_python_version = info_python_version, info_username_unix = info_username_unix)
 
 
-@app.route('/index', methods = ['POST'])
+@app.route('/main_form', methods = ['POST'])
 def main_form():
     if request.method == 'POST':
         #user = request.form.get('user_name')
@@ -109,6 +109,7 @@ def main_form():
         #user = request.args.get('user_name')
         #return redirect(url_for('success', name = user))
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
