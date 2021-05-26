60301812b1392b15f4ab12b5c39707d762332cf6
ZaytsevNS <fatalbest@mail.ru>
2021-05-26 21:28:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,46 +22,46 @@ def get_result():
     number_finish = int(request.form['number_finish'])
     number_quantity = int(request.form['number_quantity'])
     error = Markup('<br>вы не выбрали данную опцию!')
-    global product
-    product = 1
-    min_num = 0
-    sorted_list = []
+    
     if number_start > number_finish:
         number_start, number_finish = number_finish, number_start
     if number_quantity >= abs(number_start - number_finish):
-        number_quantity = max(number_start, number_finish)-1
+        number_quantity = abs(number_start - number_finish)
     
     list_numbers = []
     def get_random():
-        #global number_start
-        #global number_finish
-        #global number_quantity
         numbers = random.sample(range(number_start, number_finish + 1), k=number_quantity)
         list_numbers.extend(numbers)
         numbers_in_line = ', '.join(map(str, numbers))
         return numbers_in_line
 
     def get_product():
-        global product
+        product = 1
         if request.form.get('value_one'):
             for i in list_numbers:
                 product *= abs(i)
             return product
         else:
-            product = error
-        return product
+            return error
+        
+    def get_abs_minimum():
+        if request.form.get('value_two'):
+            min_num = min([abs(i) for i in list_numbers])
+            return min_num
+        else:
+            return error
     
-    if request.form.get('value_two'):
-        min_num = min([abs(i) for i in numbers])
-    else:
-        min_num = error
-    if request.form.get('value_three'):
-        sorted_list.extend(sorted([abs(i) for i in numbers]))
-        sorted_list = ', '.join(map(str, sorted_list))
-    else:
-        sorted_list = error
-    return render_template('result.html', name=user_name, start=number_start, finish=number_finish, quantity=number_quantity, random_num=get_random(), product_number=get_product(), min_number=min_num, sorted_number=sorted_list)
+    def get_sorted_numbers():
+        sorted_list = []
+        if request.form.get('value_three'):
+            sorted_list.extend(sorted([abs(i) for i in list_numbers]))
+            sorted_list = ', '.join(map(str, sorted_list))
+            return sorted_list
+        else:
+            return error
+    
+    return render_template('result.html', name=user_name, start=number_start, finish=number_finish, quantity=number_quantity, random_num=get_random(), product_number=get_product(), min_number=get_abs_minimum(), sorted_number=get_sorted_numbers())
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
