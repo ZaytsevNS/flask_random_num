c9b830d19602aab92911bf732a216665c15e8c4b
ZaytsevNS <fatalbest@mail.ru>
2021-05-26 20:09:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,46 +5,46 @@ import os
 
 
 app = Flask(__name__)
-   
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-    
-    
+
+
 @app.route('/get_result', methods=['GET', 'POST'])
 def get_result():
-   user_name = request.form['user_name']
-   number_start = int(request.form['number_start'])
-   number_finish = int(request.form['number_finish'])
-   number_quantity = int(request.form['number_quantity'])
-   error = Markup('<br>вы не выбрали данную опцию!')
-   product = 1
-   min_num = 0
-   sorted_list = []
-   if number_start > number_finish:
-     number_start, number_finish = number_finish, number_start
-   if number_quantity >= abs(number_start - number_finish):
-     number_quantity = max(number_start, number_finish)-1
-   numbers = random.sample(range(number_start, number_finish + 1), k=number_quantity)
-   random_number = ', '.join(map(str, numbers))
-   if request.form.get('value_one'):
-     for i in numbers:
-       product *= abs(i)
-   else:
-     product = error
-   if request.form.get('value_two'):
-     min_num = min([abs(i) for i in numbers])
-   else:
-     min_num = error
-   if request.form.get('value_three'):
-     sorted_list.extend(sorted([abs(i) for i in numbers]))
-     sorted_list = ', '.join(map(str, sorted_list))
-   else:
-     sorted_list = error
-   return render_template('result.html', name = user_name, start = number_start, finish = number_finish, quantity = number_quantity, random_num = random_number, product_number = product, \
-   min_number = min_num, sorted_number = sorted_list, info_platform = sys.platform, info_sys_description = sys.version_info, info_python_version = sys.version, info_username_unix = os.getlogin())
-   
+    user_name = request.form['user_name']
+    number_start = int(request.form['number_start'])
+    number_finish = int(request.form['number_finish'])
+    number_quantity = int(request.form['number_quantity'])
+    error = Markup('<br>вы не выбрали данную опцию!')
+    product = 1
+    min_num = 0
+    sorted_list = []
+    if number_start > number_finish:
+        number_start, number_finish = number_finish, number_start
+    if number_quantity >= abs(number_start - number_finish):
+        number_quantity = max(number_start, number_finish)-1
+    numbers = random.sample(
+        range(number_start, number_finish + 1), k=number_quantity)
+    random_number = ', '.join(map(str, numbers))
+    if request.form.get('value_one'):
+        for i in numbers:
+            product *= abs(i)
+    else:
+        product = error
+    if request.form.get('value_two'):
+        min_num = min([abs(i) for i in numbers])
+    else:
+        min_num = error
+    if request.form.get('value_three'):
+        sorted_list.extend(sorted([abs(i) for i in numbers]))
+        sorted_list = ', '.join(map(str, sorted_list))
+    else:
+        sorted_list = error
+    return render_template('result.html', name=user_name, start=number_start, finish=number_finish, quantity=number_quantity, random_num=random_number, product_number=product,
+                           min_number=min_num, sorted_number=sorted_list, info_platform=sys.platform, info_sys_description=sys.version_info, info_python_version=sys.version, info_username_unix=os.getlogin())
 
 
 if __name__ == '__main__':
