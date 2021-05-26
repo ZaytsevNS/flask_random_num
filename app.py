ca9b2d13f57572cbeaf1cc74f201051dcffff205
ZaytsevNS <fatalbest@mail.ru>
2021-05-26 16:03:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def main_form():
         #user = request.args.get('user_name')
         #return redirect(url_for('success', name = user))
 @app.route('/')
-@app.route('/index')
+#@app.route('/index')
 def index():
     return render_template('index.html')
 
